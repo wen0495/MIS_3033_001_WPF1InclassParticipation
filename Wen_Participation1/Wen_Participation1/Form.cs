372eb7e14432b2e7856b13015c3ce2aa7c3c9d3f
Ruth Wen <ruth.wen@ou.edu>
2019-02-06 11:50:48 -0600

    participation

---
+++ Wen_Participation1/Wen_Participation1/Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wen_Participation1
+{
+    class Form
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public Form()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public Form(string name, string address, int zipcode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipcode;
+        }
+
+        public override string ToString()
+        {
+            string result = $"Hello {Name}, your address is {Address}, and zipcode is {ZipCode}";
+            return result;
+        }
+    }
+}
---
+++ Wen_Participation1/Wen_Participation1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wen_Participation1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Form> submission = new List<Form>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //string userName = name.Text;
+            //string userAddress = address.Text;
+            int userZipcode = Convert.ToInt32(zipcode.Text);
+            Form submission1 = new Form();
+            submission1.Name = name.Text;
+            submission1.Address = address.Text;
+            submission1.ZipCode = userZipcode;
+
+            lst.Items.Add(submission1);
+
+        }
+    }
+}
