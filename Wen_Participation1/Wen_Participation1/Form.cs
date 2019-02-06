e863e52c8a9f842106fc522826c991d952cfaf76
Ruth Wen <ruth.wen@ou.edu>
2019-02-06 16:00:26 -0600

    Office Hour - Tim explain Class Notes

--- Wen_Participation1/Wen_Participation1/Form.cs
+++ Wen_Participation1/Wen_Participation1/Form.cs
@@ -11,12 +11,17 @@ namespace Wen_Participation1
         public string Name { get; set; }
         public string Address { get; set; }
         public int ZipCode { get; set; }
+        public double one { get; set; }
+        public double two { get; set; }
+
 
         public Form()
         {
             Name = "";
             Address = "";
             ZipCode = 0;
+            one = 0;
+            two = 0;
         }
 
         public Form(string name, string address, int zipcode)
@@ -28,8 +33,15 @@ namespace Wen_Participation1
 
         public override string ToString()
         {
-            string result = $"Hello {Name}, your address is {Address}, and zipcode is {ZipCode}";
+            string result = $"Hello {Name}, your address is {Address}, and zipcode is {ZipCode}{one} {two} {qwe()}";
+            //result = $"{two}";
             return result;
+            string concat = "this" + Name + " is how you " + Address + " ";
+        }
+
+        public double qwe()
+        {
+            return one + two;
         }
     }
 }
--- Wen_Participation1/Wen_Participation1/MainWindow.xaml.cs
+++ Wen_Participation1/Wen_Participation1/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace Wen_Participation1
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Form> submission = new List<Form>();
+        List<Form> submissions = new List<Form>();
+        Random rand = new Random();
 
         public MainWindow()
         {
@@ -36,13 +37,44 @@ namespace Wen_Participation1
         {
             //string userName = name.Text;
             //string userAddress = address.Text;
-            int userZipcode = Convert.ToInt32(zipcode.Text);
+            for (int i = 1; i < 8; i++)
+            {
+                int userZipcode = Convert.ToInt32(zipcode.Text);
+                Form submission1 = new Form();
+                submission1.Name = name.Text;
+                submission1.Address = address.Text;
+                submission1.ZipCode = userZipcode; //Convert.ToInt32(zipcode.Text);
+                submission1.one = 10;
+                submission1.two = rand.Next(1, 6);
+                submissions.Add(submission1);
+            }
+
+            foreach(Form submission in submissions)
+            {
+                lst.Items.Add(submission);
+            }
+            
+
+        }
+
+        private void lst_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string count = lst.SelectedItem.ToString();
+
+            double total = count.Length;
+           
+
             Form submission1 = new Form();
             submission1.Name = name.Text;
             submission1.Address = address.Text;
-            submission1.ZipCode = userZipcode;
+            submission1.ZipCode = Convert.ToInt32(zipcode.Text); //Convert.ToInt32(zipcode.Text);
+            submission1.one = total;
+            submission1.two = rand.Next(1, 6);
+            lst.Items.Add($"{submission1.one} {submission1.two} {submission1.qwe()}");
+            lst.Items.Add(submission1.two);
+            lst.Items.Add(submission1.qwe());
 
-            lst.Items.Add(submission1);
+            double i = 1;
 
         }
     }
